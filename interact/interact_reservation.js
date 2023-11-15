8bb7d183640058b894ce1f96ec809d58bac5b298
oliver-h522 <7422723+oliverh522@user.noreply.gitee.com>
2023-11-15 11:23:15 +0800

    refactor

--- interact/interact_reservation.js
+++ interact/interact_reservation.js
@@ -2,14 +2,14 @@ const hre = require("hardhat");
 
 async function main() {
   try {
-    // Get the ContractFactory of your contract
     const ChargingReservation = await hre.ethers.getContractFactory("ChargingReservation");
 
-    // Connect to the deployed contract
+    //replace with your smart contract address on your localhost network
     const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"; 
     const reservation = await ChargingReservation.attach(contractAddress);
-    // Greet from your contract
-    console.log("the message from smat contract: ChargingReservation===>",(await reservation.greet()).toString());
+    
+    const greet = await reservation.greet();
+    console.log("the message from smat contract: ChargingReservation===>",(greet).toString());
 
   } catch (error) {
     console.error(error);
