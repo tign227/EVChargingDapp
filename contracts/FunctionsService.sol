2931e8a6a4e02284d0277d2a0ba64f3d43017f65
oliver-h522 <7422723+oliverh522@user.noreply.gitee.com>
2023-11-21 15:47:19 +0800

    add comments and refactor

--- contracts/FunctionsService.sol
+++ contracts/FunctionsService.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.19;
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 
+/**
+ * @title FunctionsService
+ * @dev This contract interacts with Chainlink's Functions service to send requests to external APIs.
+ * The `url` parameter specifies the address of the target API, and the `path` parameter indicates the
+ * JSONPath path in the API response. The contract handles the asynchronous return of the API through
+ * the `fulfill` callback function.
+ */
 contract FunctionsService is ChainlinkClient, ConfirmedOwner {
     using Chainlink for Chainlink.Request;
-
-    string public result;
 
     bytes32 private jobId;
     uint256 private fee;
@@ -19,6 +24,10 @@ contract FunctionsService is ChainlinkClient, ConfirmedOwner {
         string result
     );
 
+    /**
+     * @dev Constructor function to initialize the Oracle contract.
+     * It sets the Chainlink token, Oracle address, job ID, and fee.
+     */
     constructor() ConfirmedOwner(msg.sender) {
         setChainlinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789);
         setChainlinkOracle(0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD);
@@ -26,6 +35,13 @@ contract FunctionsService is ChainlinkClient, ConfirmedOwner {
         fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
     }
 
+    /**
+     * @dev Initiates a Chainlink request to fetch external data.
+     * @param requestType The type of the request：Account & Reservation
+     * @param url The URL of the external API.
+     * @param path The JSONPath to extract the desired data from the API response.
+     * @return requestId The unique identifier for the Chainlink request.
+     */
     function request(
         string memory requestType,
         string memory url,
@@ -44,18 +60,19 @@ contract FunctionsService is ChainlinkClient, ConfirmedOwner {
     }
 
     /**
-     * Receive the response in the form of string
+     * @notice Callback function to handle the asynchronous return of the API.
+     * This function is triggered when the API response is received.
+     * Implement the logic to process the API response here.
      */
     function fulfill(
         bytes32 _requestId,
         string memory _result
     ) public recordChainlinkFulfillment(_requestId) {
         emit RequestMade(_requestId, requestTypes[_requestId], _result);
-        result = _result;
     }
 
     /**
-     * Allow withdraw of Link tokens from the contract
+     * @dev Allows the owner to withdraw any remaining LINK tokens from the contract.
      */
     function withdrawLink() public onlyOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
--- test/ChargingReservation.test.js
+++ test/ChargingReservation.test.js
@@ -3,20 +3,19 @@ const { expect, assert } = require("chai");
 
 describe("ChargingReservation", function () {
   it("Make a reservation", async function () {
-    const [reserverAddress, plateLicense] = await ethers.getSigners();
+    const FunctionsService = await ethers.getContractFactory("FunctionsService");
+    const service = await FunctionsService.deploy();
+
     const ChargingReservation = await ethers.getContractFactory("ChargingReservation");
-    const reservation = await ChargingReservation.deploy();
+    const reservation = await ChargingReservation.deploy(service.target);
 
-    const positionX = 43;
-    const positionY = 567;
-    const chargingStationID = 1;
-    const chargingStationAddress = "Mock Address"
-    const chargingStationName = "Mock Name"
-    const startTime = Math.floor(Date.now() / 1000) + 3600; // start reservation 1 hour from now
-    const endTime = startTime + 7200; // end reservation 2 hours from start
-
-    const tx = await reservation.makeReservation(reserverAddress, plateLicense, positionX, positionY, chargingStationID,chargingStationAddress, chargingStationName, startTime, endTime);
-    assert(tx, "200");
+    url = "http://endpoint-dun.vercel.app/api/reservation";
+    path = "message,reservationCode";
+    const requestId = await reservation.makeReservation(
+      url,
+      path,
+    );
+    console.log(requestId.toString());
   });
 });
 
