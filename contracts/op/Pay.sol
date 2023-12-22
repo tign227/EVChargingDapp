b5ba1136f61243d7f804ca2d3c423319f9e066c9
tign227 <tingh5370@gmail.com>
2023-12-22 21:40:07 +0800

    deal with failure payment

--- contracts/op/Pay.sol
+++ contracts/op/Pay.sol
@@ -1,11 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-contract Payment {
-    
-    function pay(address offical, uint256 amount) external returns (bool) {
-        require(offical != address(0), "address = 0");
-        (bool success, ) = offical.call{value: amount}("");
-        return success;
+contract Pay {
+    mapping(address => uint256) failureRecords;
+
+    error FailedPayment(address user, address official, uint256 amount);
+
+    bool private locked;
+
+    modifier ReentrancyGuard() {
+        require(locked);
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    function pay(address official, uint256 amount) external ReentrancyGuard {
+        require(official != address(0), "address = 0");
+        require(official.code.length == 0, "contract address");
+        (bool success, ) = official.call{value: amount}("");
+        if (!success) {
+            failureRecords[msg.sender] = amount;
+            revert FailedPayment(msg.sender, official, amount);
+        }
     }
 }
